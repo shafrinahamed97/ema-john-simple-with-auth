78012c5064f290b64cac88d1cb36d96fd478363d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-10-19 10:58:40 +0600

    added user name show in display

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 export const AuthContext = createContext();
 
@@ -16,7 +16,20 @@ const UserContext = ({children}) => {
     return signInWithEmailAndPassword(auth, email, password);
   }
 
-    const authInfo = {user, createUser, signIn}
+  const logOut=  () =>{
+    return signOut(auth);
+  }
+
+  useEffect(() =>{
+  const unSubscribe =   onAuthStateChanged(auth, currentUser =>{
+      console.log('current user inside state change', currentUser);
+      setUser(currentUser);
+    });
+    return () => unSubscribe();
+
+  }, [])
+
+    const authInfo = {user, createUser, signIn, logOut}
     return (
         <AuthContext.Provider value={authInfo}>
           {children}
